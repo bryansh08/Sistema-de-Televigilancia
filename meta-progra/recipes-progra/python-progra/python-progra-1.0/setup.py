3c03a6a444a46f21d31bc7bf3b07faf7abfab407
pauclari <66702741+pauclari@users.noreply.github.com>
2020-06-29 11:52:59 -0600

    Create setup.py

---
+++ meta-progra/recipes-progra/python-progra/python-progra-1.0/setup.py
@@ -0,0 +1,13 @@
+import sys
+from setuptools import setup
+setup(
+    name = "progra",
+    version = "0.1",
+    packages = ["progra"],
+    author = "Paula",
+    author_email = "pau@example.com",
+    description  = "Captura de video y deteccion de videos",
+    license = "MIT",
+    keywords = "example",
+    url = "",
+)
